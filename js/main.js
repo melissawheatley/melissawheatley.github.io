a87a0551a94ee0130bfed87d238bf1edea3643e4
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-04-20 05:21:07 -0500

    template modifications.

--- js/main.js
+++ js/main.js
@@ -42,9 +42,9 @@
   // Hide navbar when modals trigger
   $('.portfolio-modal').on('show.bs.modal', function(e) {
     $(".navbar").addClass("d-none");
-  })
+  });
   $('.portfolio-modal').on('hidden.bs.modal', function(e) {
     $(".navbar").removeClass("d-none");
-  })
+  });
 
 })(jQuery); // End of use strict
